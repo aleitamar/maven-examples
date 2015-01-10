7d6e7e665db13a20b32e7a66809f707075d475eb
Paul Hammant <paul@hammant.org>
2015-01-10 16:00:46 -0500

    comments added

--- code-coverage-jacoco/testimpact.sh
+++ code-coverage-jacoco/testimpact.sh
@@ -3,15 +3,30 @@
 mkdir target
 mkdir src/integration-test/meta
 
+# list of integration tests (unit tests would be in src/tests)
+
 ack -l "@Test" src/integration-test > target/integration_test_list.txt
+
+# Process them one at a time
 
 for fn in `cat target/integration_test_list.txt`; do
 	testname=$(echo "$fn" | sed 's#src/integration-test/java/##')
 	testname=$(echo "$testname" | sed 's#/#.#g')
+
+	# One maven invocation per test (so we can get focused coverage)
+
     mvn -Dit.test=$testname clean verify -P integration-test
+
+    # TODO Maven could fail per test, which is half expected
+
 	metapath="${fn/java/meta}"
 	mkdir -p ${metapath%/*}
 	python testimpact_subset_csv.py > $metapath 
 done
 
+# Turn many files into map of the total picture
+# that's source vs tests (instead of tests vs sources)
+
 ack --noheading "." src/integration-test/meta/ | sed 's#src/integration-test/meta/##g' | sed 's/\.java/ /' | sed 's#/#.#g' | sed 's/:[[:digit:]]://' | awk ' { t = $1; $1 = $2; $2 = t; print; } ' | sort > src/integration-test/impact-map.txt
+
+# TODO - check in potentially changed cross references files here
--- code-coverage-jacoco/tests.py
+++ code-coverage-jacoco/tests.py
@@ -13,6 +13,8 @@ for statusLine in iter(gitStatus):
             if mapentry.startswith(sourcefile + " "):
                 tests.add(mapentry[mapentry.index(" ")+1 : len(mapentry)-1])
         testmap.close()
+    # TODO - tests that are new should be added to the list
+    # TODO - tests might have been deleted, yet could still be in the map.
 gitStatus.close()
 
 if len(tests) > 0:
